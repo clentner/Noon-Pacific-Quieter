5b261d3b18da1c49fa9a08b617a5e9ca3fcc9989
Chris Lentner <cjl8505@rit.edu>
2016-06-23 01:14:33 -0400

    tabs to spaces

--- volume.js
+++ volume.js
@@ -1,19 +1,19 @@
 // http://stackoverflow.com/a/9517879/3707721
 function injectScript(f, a) {
-	var actualCode = '(' + f + ')(' + JSON.stringify(a) + ');';
-	var script = document.createElement('script');
-	script.textContent = actualCode;
-	(document.head||document.documentElement).appendChild(script);
-	script.parentNode.removeChild(script);
+    var actualCode = '(' + f + ')(' + JSON.stringify(a) + ');';
+    var script = document.createElement('script');
+    script.textContent = actualCode;
+    (document.head||document.documentElement).appendChild(script);
+    script.parentNode.removeChild(script);
 }
 
 chrome.storage.sync.get({volume: 1}, function(items){
-	injectScript(function(volume){
-		// move the slider bar itself
-		angular.element('.vslider').slider('setValue', '' + volume);
-		// then fake the slide event as if the user had moved the slider
-		var event = $.Event('slide');
-		event.value = volume;
-		$('.vslider').trigger(event);
-	}, items.volume);
+    injectScript(function(volume){
+        // move the slider bar itself
+        angular.element('.vslider').slider('setValue', '' + volume);
+        // then fake the slide event as if the user had moved the slider
+        var event = $.Event('slide');
+        event.value = volume;
+        $('.vslider').trigger(event);
+    }, items.volume);
 });
